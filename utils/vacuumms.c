76bcc8e85784eb447afa7e7e2a806bb8a618136d
Frank Willmore <willmore@anl.gov>
2021-12-22 23:00:35 +0000

    add several utilities plus vacuumms executable to return a version.

---
+++ utils/vacuumms.c
@@ -0,0 +1,8 @@
+int main()
+{
+    // put these in a version file to be read
+    int major = 1;
+    int minor = 1;
+    int patch = 0;
+    printf("VACUUMMS version %d.%d.%d\n\n", major, minor, patch);
+}
