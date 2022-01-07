23ceb4916cbb1fe825bb9eea343438c34bfe91c0
Frank Willmore <frankwillmore@gmail.com>
2022-01-07 17:02:43 +0000

    adding macros for VERSION

--- utils/vacuumms.c
+++ utils/vacuumms.c
@@ -1,8 +1,14 @@
+#include <stdio.h>
+
+#define VACUUMMS_MAJOR_VERSION 1
+#define VACUUMMS_MINOR_VERSION 1
+#define VACUUMMS_PATCH_VERSION 0
+
 int main()
 {
     // put these in a version file to be read
-    int major = 1;
-    int minor = 1;
-    int patch = 0;
+    int major = VACUUMMS_MAJOR_VERSION;
+    int minor = VACUUMMS_MINOR_VERSION;
+    int patch = VACUUMMS_PATCH_VERSION;
     printf("VACUUMMS version %d.%d.%d\n\n", major, minor, patch);
 }
