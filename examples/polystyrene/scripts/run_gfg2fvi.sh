33a877ca791cc6a43ea04acc559496a3491c4b1b
Frank Willmore <frankwillmore@gmail.com>
2022-08-30 11:54:07 -0600

    update scripts to show resolution of 1024

--- examples/polystyrene/scripts/run_gfg2fvi.sh
+++ examples/polystyrene/scripts/run_gfg2fvi.sh
@@ -1,8 +1,7 @@
-#gdb --args gfg2fvi \
 gfg2fvi \
      -box 87.57884525964772 87.57884525964772 87.57884525964772 \
      -potential 69 \
-     -resolution 256 \
+     -resolution 1024 \
      < ../generated_files/PS.gfg \
      > PS1024.fvi
 
