fba9073d501fd75b7026a86839779fded3fa55da
Frank Willlmore <frankwillmore@gmail.com>
2022-12-15 15:10:23 +0800

    fleshing out pipes idea some

--- python/vacuumms.py
+++ python/vacuumms.py
@@ -8,11 +8,33 @@ def function4():
 def file_eater(file="x"):
     print("eating file " + file)
 
+# Need to make a vacuumms object subclass, that has input and output streams, and an action
+
+def open(file="")
+    # Open a file and return a pipe
+
+def close(file="")
+    # Drain a pipe to a file
+
 # open the file in dst2hst. Can fix stderr later. 
-#def dst2hst():
-#    f=open('x.dst', "r")
-#    d2h=subprocess.Popen("dst2hst", stdout=subprocess.PIPE, stdin=f)
+def dst2hst():
+    f=open('x.dst', "r")
+    d2h=subprocess.Popen("dst2hst", stdout=subprocess.PIPE, stdin=f)
+
+# Just dump pipe to stdout
 #    for line in d2h.stdout:
 #        print(line.rstrip().decode('ASCII'))
-#
+
     
+def VACUUMMS:
+# stuff
+    
+    # class members
+    input
+    output
+    error
+    def process():
+    parameters[]
+
+def dst2hst(VACUUMMS):
+# stuff
