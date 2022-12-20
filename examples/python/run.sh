e20e25115b7a7cdae6275dc8f8310bef8cd493ec
Frank Willlmore <frankwillmore@gmail.com>
2022-12-20 12:17:18 +0800

    initial bindings for pddx and types, still need to capture output

---
+++ examples/python/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import vacuumms
+
+#f=vacuumms.v_open_file("xyz.dat")
+#vacuumms.v_drain_pipe(pipe=f, file="xyz.out")
+#outfile=open("file.out", "a")
+
+# create a gfg object from a file
+fcc = vacuumms.gfg(input_file="fcc.gfg", dims=[4.242640687119285, 4.242640687119285, 4.242640687119285])
+
+# check what was read
+fcc.dump()
+
+# start a process to run pddx, and return .cav object
+my_cavs=vacuumms.pddx(fcc)
+
+
+
+
--- python/vacuumms.py
+++ python/vacuumms.py
@@ -1,20 +1,26 @@
+# This is vacuumms.py, defines the package 
 
-def function3():
-    print("This is function3")
-
-def function4():
-    print("This is function4")
+import csv
+import subprocess
 
 def file_eater(file="x"):
     print("eating file " + file)
 
 # Need to make a vacuumms object subclass, that has input and output streams, and an action
 
-def open(file="")
+def v_open_file(file="/dev/null"):
+    retval = open(file)
+    return retval
+    
     # Open a file and return a pipe
 
-def close(file="")
-    # Drain a pipe to a file
+
+def v_drain_pipe(pipe="", file="/dev/null"):
+    # Drain a pipe to a file ?
+    f=open("file", "w")
+    for line in pipe.stdout:
+        f.write(line)
+#        print(line.rstrip().decode('ASCII'))
 
 # open the file in dst2hst. Can fix stderr later. 
 def dst2hst():
@@ -26,15 +32,87 @@ def dst2hst():
 #        print(line.rstrip().decode('ASCII'))
 
     
-def VACUUMMS:
-# stuff
-    
-    # class members
-    input
-    output
-    error
-    def process():
-    parameters[]
+# superclass for all data objects
+class VACUUMMS:
+    def __init__(self, input_file, dims):
+        print("constructing VACUUMMS object")
+        self.box_x=dims[0]
+        self.box_y=dims[1]
+        self.box_z=dims[2]
 
-def dst2hst(VACUUMMS):
-# stuff
+    def dump(self):
+        print("box_x=" + str(self.box_x)) 
+        print("box_y=" + str(self.box_y)) 
+        print("box_z=" + str(self.box_z)) 
+    
+
+class cav(VACUUMMS):
+    def __init__(self, input_file, dims):
+        super().__init__(input_file, dims)
+        print("constructing VACUUMMS::cav object")
+
+        with open(input_file, "r", encoding="utf8") as cav_file:
+            reader = csv.reader(cav_file, delimiter="\t")
+            for row in reader:
+                (x, y, z, d) = row
+                print(f"x={x} y={y} z={z} d={d}")
+        
+        print("Read " + str(input_file))
+
+
+class gfg(VACUUMMS):
+    def __init__(self, input_file, dims):
+        super().__init__(input_file, dims)
+        print("constructing VACUUMMS::gfg object from "+input_file + " with dims = " + str(dims))
+        self.dims = dims
+        self.gfg_list = []
+
+        with open(input_file, "r", encoding="utf8") as gfg_file:
+            reader = csv.reader(gfg_file, delimiter="\t")
+            for row in reader:
+                (x, y, z, sigma, epsilon) = row
+                self.gfg_list.append(row);
+
+    def dump(self):
+#        print("Dumping gfg_list")
+#        print(self.gfg_list)
+        for row in self.gfg_list:
+            (x, y, z, sigma, epsilon) = row
+            print(f"x={x} y={y} z={z} sigma={sigma} epsilon={epsilon}")
+
+#pddx usage:	-box [ 6.0 6.0 6.0 ]
+#		-seed [ 1 ]
+#		-characteristic_length [ 1.0 ] if in doubt, use largest sigma/diameter
+#		-characteristic_energy [ 1.0 ] if in doubt, use largest energy/epsilon
+#		-n_steps [ 1000000 ] maximum before giving up
+#		-n_threads [ 1 ] 
+#		-show_steps (includes steps taken as final column)
+#		-verlet_cutoff [ 100.0 ]
+#		-n_samples [ 1 ]
+#		-volume_sampling 
+#		-include_center_energy 
+#		-min_diameter [ 0.0 ] 0.0 will give all 
+
+def pddx(gfg, n_steps=1000000, n_threads=1, verlet_cutoff=100.0, n_samples=1, min_diameter=0.0):
+#    f=open('x.dst', "r")
+#    d2h=subprocess.Popen("dst2hst", stdout=subprocess.PIPE, stdin=f)
+#    wc = subprocess.Popen(['wc', '-l'], stdin=subprocess.PIPE)
+
+    pddx_arglist = ['pddx', '-box ', str(gfg.dims[0]), str(gfg.dims[1]), str(gfg.dims[2]) ]
+    pddx_arglist.append("-n_steps " + str(n_steps))
+    print("FTW pddx_arglist: " + str(pddx_arglist))
+
+    # open the process for pddx, write info, and capture output
+    cav = subprocess.Popen(pddx_arglist, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    for row in gfg.gfg_list:
+        (x, y, z, sigma, epsilon) = row
+#        print(f"x={x} y={y} z={z} sigma={sigma} epsilon={epsilon}", file=cav.stdin)
+        cav.stdin.write(bytes((str(x)+"\t"+str(y)+"\t"+str(z)+"\t"+str(sigma)+"\t"+str(epsilon))
+#        cav.stdin.write(bytes("abc", 'utf-8'))
+
+    # capture output
+    for line in cav.stdout:
+#        print(line.rstrip().decode('ASCII'))
+        print(line)
+
+
