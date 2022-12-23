a6e413397387ee4d493db4dc2d06734ef2870829
Frank Willlmore <frankwillmore@gmail.com>
2022-12-23 16:31:55 +0800

    added cls and fvi datatypes

--- python/vacuumms_types.py
+++ python/vacuumms_types.py
@@ -59,7 +59,9 @@ class cav(VACUUMMS):
             print(f"{record.x}\t{record.y}\t{record.z}\t{record.d}")
 
     # write contents to flat file (traditional)
-    def to_file(self, filename="/dev/null"):
+#    def to_file(self, filename="/dev/null"):
+    def to_file(self, filename=None):
+        if (filename == None): filename=str(self) + ".cav"
         with open(filename, "w") as file:
             for record in self.list:
                 print(f"{record.x}\t{record.y}\t{record.z}\t{record.d}", file=file)
@@ -84,5 +86,81 @@ class gfg(VACUUMMS):
         super().dump() # Dump PVT, box, etc
         for record in self.gfg_list:
             print(f"{record.x}\t{record.y}\t{record.z}\t{record.sigma}\t{record.epsilon}")
+
+
+class cls_record():
+    def __init__(self, record):
+        (self.cluster, self.x, self.y, self.z, self.d) = record
+    def __str__(self):
+        tab = "\t"
+        return str(self.cluster) + tab + str(self.x) + tab + str(self.y) + tab + str(self.z) + tab + str(self.d)
+
+class cls(VACUUMMS):
+
+    def __init__(self, input_source, box):
+        super().__init__(box)
+        print("constructing VACUUMMS::cls object")
+
+        # Capture the output stream instead of a file to create the cls object
+
+        if (str(type(input_source)) == "<class '_io.BufferedReader'>"):
+            for line in input_source:
+                self.list.append(cls_record(line.decode().rstrip().split("\t")))
+
+        else: # otherwise treat input_source as a file
+            with open(input_file, "r", encoding="utf8") as cls_file:
+                reader = csv.reader(cls_file, delimiter="\t")
+                for row in reader:
+                    self.list.append(cls_record(row))
+
+    def dump(self):
+        super().dump() # Dump PVT, box, etc
+        for record in self.list:
+            print(f"{record.cluster}\t{record.x}\t{record.y}\t{record.z}\t{record.d}")
+
+    # write contents to flat file (traditional)
+    def to_file(self, filename=None):
+        if (filename == None): filename=str(self) + ".cls"
+        with open(filename, "w") as file:
+            for record in self.list:
+                print(f"{record.cluster}\t{record.x}\t{record.y}\t{record.z}\t{record.d}", file=file)
+
+
+class fvi_record():
+    def __init__(self, record):
+        (self.x, self.y, self.z, self.energy) = record
+    def __str__(self):
+        tab = "\t"
+        return str(self.x) + tab + str(self.y) + tab + str(self.z) + tab + str(self.energy)
+
+class fvi(VACUUMMS):
+
+    def __init__(self, input_source, box):
+        super().__init__(box)
+        print("constructing VACUUMMS::fvi object")
+
+        # Capture the output stream instead of a file to create the fvi object
+
+        if (str(type(input_source)) == "<class '_io.BufferedReader'>"):
+            for line in input_source:
+                self.list.append(fvi_record(line.decode().rstrip().split("\t")))
+
+        else: # otherwise treat input_source as a file
+            with open(input_file, "r", encoding="utf8") as fvi_file:
+                reader = csv.reader(fvi_file, delimiter="\t")
+                for row in reader:
+                    self.list.append(fvi_record(row))
+
+    def dump(self):
+        super().dump() # Dump PVT, box, etc
+        for record in self.list:
+            print(f"{record.x}\t{record.y}\t{record.z}\t{record.energy}")
+
+    # write contents to flat file (traditional)
+    def to_file(self, filename=None):
+        if (filename == None): filename=str(self) + ".fvi"
+        with open(filename, "w") as file:
+            for record in self.list:
+                print(f"{record.x}\t{record.y}\t{record.z}\t{record.energy}", file=file)
 
 
