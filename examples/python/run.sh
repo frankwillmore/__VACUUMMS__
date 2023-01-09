27aedf434e1129bad9a2cec3642dd25935ec29ba
Frank Willlmore <frankwillmore@gmail.com>
2023-01-09 13:38:16 -0600

    later

--- examples/python/run.sh
+++ examples/python/run.sh
@@ -5,17 +5,13 @@ import vacuumms
 # create a gfg object from a file
 fcc = vacuumms.gfg(input_file="fcc.gfg", box=[4.242640687119285, 4.242640687119285, 4.242640687119285])
 
-# Check what was read
-#fcc.dump()
-
-# start a process to run ddx, and return .cav object
+# run ddx, and return a cav object
 my_cavs=vacuumms.ddx(fcc, n=5)
 
-# Dump the result
-my_cavs.dump()
-#for line in my_cavs.cav_list:
-#    print(str(line))
+# display the result, with metadata
+# my_cavs.dump()
 
+# write result to file
 my_cavs.to_file("fcc.cav")
 
 
